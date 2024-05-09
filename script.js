27906dfc7cedb0c49dd60f85d6f2dbb355e8d52f
LucasSobrinh0 <lucas.sobrinho.crn@gmail.com>
2024-05-09 15:41:27 -0300

    Adicionando páginas de artigo

---
+++ script.js
@@ -0,0 +1,19 @@
+let phrases = [
+    "Aprenda de forma rápida e iterativa",
+    "Leia de forma simples e direta",
+    "Encontre novos horizontes"
+];
+let currentIndex = 0;
+
+function changeText() {
+    const textElement = document.querySelector('.animated-text');
+    currentIndex = (currentIndex + 1) % phrases.length;
+    textElement.style.opacity = '0';
+    setTimeout(() => {
+        textElement.innerText = phrases[currentIndex];
+        textElement.style.opacity = '1';
+    }, 500); // Espera meio segundo para mudar a opacidade e iniciar a nova frase
+}
+
+// Inicializa a primeira transição imediatamente após a página carregar
+setInterval(changeText, 1500);
